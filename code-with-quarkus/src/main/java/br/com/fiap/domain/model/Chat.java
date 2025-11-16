a9a8ace4f266049a467f4d0424003aa447146ffc
JonasSantoos <jscom3121@gmail.com>
2025-11-16 17:46:08 -0300

    Classe Chat criada

---
+++ code-with-quarkus/src/main/java/br/com/fiap/domain/model/Chat.java
@@ -0,0 +1,62 @@
+package br.com.fiap.domain.model;
+
+import java.time.LocalDate;
+
+public class Chat {
+
+    private Long idChat;
+    private String titulo;
+    private LocalDate dataCriacao;
+    private LocalDate dataAtualizacao;
+    private Usuario usuario;
+
+    public Chat(Long idChat, String titulo, LocalDate dataCriacao, LocalDate dataAtualizacao, Usuario usuario) {
+        this.idChat = idChat;
+        this.titulo = titulo;
+        this.dataCriacao = dataCriacao;
+        this.dataAtualizacao = dataAtualizacao;
+        this.usuario = usuario;
+    }
+
+    public Chat() {}
+
+    public Long getIdChat() {
+        return idChat;
+    }
+
+    public void setIdChat(Long idChat) {
+        this.idChat = idChat;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public LocalDate getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(LocalDate dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+}
