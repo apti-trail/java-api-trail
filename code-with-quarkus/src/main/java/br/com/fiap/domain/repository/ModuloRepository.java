6fdcc3d691ccb2f7a5be3d2c4c6d079b1bc97e85
JonasSantoos <jscom3121@gmail.com>
2025-11-17 20:37:50 -0300

    Criação da classe ModuloRepository

---
+++ code-with-quarkus/src/main/java/br/com/fiap/domain/repository/ModuloRepository.java
@@ -0,0 +1,17 @@
+package br.com.fiap.domain.repository;
+
+import br.com.fiap.domain.model.Mensagens;
+import br.com.fiap.domain.model.Modulo;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ModuloRepository {
+
+    Modulo salvarModulo(Modulo modulo);
+    void excluir(Modulo modulo);
+
+    List<Modulo> listarTodos();
+    Optional<Modulo> buscarPorId(Long id);
+
+}
