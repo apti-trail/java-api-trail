1f676bd5d6de46065a474fe04cd04a95334f2ba6
JonasSantoos <jscom3121@gmail.com>
2025-11-17 20:05:31 -0300

    Criação da classe AnotacaoRepository

---
+++ code-with-quarkus/src/main/java/br/com/fiap/domain/repository/AnotacaoRepository.java
@@ -0,0 +1,17 @@
+package br.com.fiap.domain.repository;
+
+import br.com.fiap.domain.model.Anotacao;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AnotacaoRepository {
+
+    Anotacao salvar(Anotacao anotacao); //Não coloquei editar, pois esse métod serve para criar E editar
+    void excluirAnotacao(Long id);
+
+    List<Anotacao> listarTodas();
+    Optional<Anotacao> buscarPorId(Long id);
+
+
+}
