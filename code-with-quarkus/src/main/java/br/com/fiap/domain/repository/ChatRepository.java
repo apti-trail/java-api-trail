4849590f55d732bb2cc089d63fe7c68e2fe0c3c0
JonasSantoos <jscom3121@gmail.com>
2025-11-17 20:08:36 -0300

    Criação da classe ChatRepository

---
+++ code-with-quarkus/src/main/java/br/com/fiap/domain/repository/ChatRepository.java
@@ -0,0 +1,16 @@
+package br.com.fiap.domain.repository;
+
+import br.com.fiap.domain.model.Chat;
+import java.util.List;
+import java.util.Optional;
+
+public interface ChatRepository {
+
+    ChatRepository salvarChat(Chat chat);
+    void excluirChat(Long id);
+
+    List<Chat> listarTodos();
+    Optional<Chat> buscarPorId(Long id);
+
+
+}
